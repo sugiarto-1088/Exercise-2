42ee5b09ffd22e1b1e67198a4418945c584955ae
sugiarto-1088 <fahrisugiarto1088@gmail.com>
2021-03-19 00:46:38 +0800

    editing code : - date - downloading data - make sure that data downloading go to folder data with no bashscript "cd"

--- main.sh
+++ main.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
 echo "Fahri"
-echo "18 Maret 2021"
+echo "19 Maret 2021"
 echo "Raster Processing with GDAL"
+mkdir -p data output
+echo "Downloading data"
+wget https://github.com/GeoScripting-WUR/IntroToRaster/raw/gh-pages/data/gewata.zip
+unzip gewata.zip
+find ./*.tif
+cp ./*.tif input.tif
+cp input.tif -t data
+rm ./*.tif
+rm ./*.zip
