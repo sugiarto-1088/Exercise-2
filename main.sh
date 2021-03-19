a98984d625957e1e7973efb512d9608f94b02d59
sugiarto-1088 <fahrisugiarto1088@gmail.com>
2021-03-20 00:45:00 +0800

    - update date 20 Maret 2021 - continue a task :   - add line fn=data...   - add line echo "The..."   - add line outfn=output...   - add line echo "The output.."   - add line echo "Calculate NDVI.."   - add line gdal_calc...

--- main.sh
+++ main.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "Fahri"
-echo "19 Maret 2021"
+echo "20 Maret 2021"
 echo "Raster Processing with GDAL"
 mkdir -p data output
 echo "Downloading data"
@@ -11,3 +11,9 @@ cp ./*.tif input.tif
 cp input.tif -t data
 rm ./*.tif
 rm ./*.zip
+fn=data/input.tif
+echo "The input file : $fn "
+outfn=output/ndvi.tif
+echo "The output file : $outfn"
+echo "Calculate NDVI"
+gdal_calc.py -A $fn --A_band=4 -B $fn --B_band=3 --outfile=$outfn --calc="(A.astype(float)-B)/(A.astype(float)+B)" --type='Float32'
