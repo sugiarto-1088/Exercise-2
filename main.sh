a956554ca1ae565af5fcf61630012e2bbef003db
sugiarto-1088 <fahrisugiarto1088@gmail.com>
2021-03-23 23:11:21 +0800

    Code Progress with Bash "Raster Processing with GDAL" -Update date 23 Maret 2021 -Edit echo -Add line in step 2, step 3, step 4 -Finished (I hope)

--- main.sh
+++ main.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 echo "Fahri"
-echo "20 Maret 2021"
-echo "Raster Processing with GDAL"
+echo "23 Maret 2021"
+echo "--Exercise 2 Raster Processing with GDAL--"
 mkdir -p data output
-echo "Downloading data"
+echo "Step 1 : Downloading data"
 wget https://github.com/GeoScripting-WUR/IntroToRaster/raw/gh-pages/data/gewata.zip
 unzip gewata.zip
 find ./*.tif
@@ -15,5 +15,16 @@ fn=data/input.tif
 echo "The input file : $fn "
 outfn=output/ndvi.tif
 echo "The output file : $outfn"
-echo "Calculate NDVI"
+echo "Step 2 : Calculate NDVI"
 gdal_calc.py -A $fn --A_band=4 -B $fn --B_band=3 --outfile=$outfn --calc="(A.astype(float)-B)/(A.astype(float)+B)" --type='Float32'
+echo "Step 3 : Resampling to 60 m"
+gdalwarp -tr 60 60 $outfn ndvi.tif
+rm $outfn
+cp ndvi.tif -t output
+rm ./*tif
+echo "Step 4 : Reprojection"
+gdalwarp -t_srs EPSG:4326 $outfn ndvi.tif
+rm $outfn
+cp ndvi.tif -t output
+rm ./*tif
+echo "Finished"
